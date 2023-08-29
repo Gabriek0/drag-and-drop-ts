f269858e2f617abcb0f0ee413fc8034667e5c9bb
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-08-28 21:42:59 -0300

    feat: render project-input template on screen

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-// Code goes here!
+class ProjectInput {
+  element: HTMLFormElement;
+  hostElement: HTMLDivElement;
+  templateElement: HTMLTemplateElement;
+
+  constructor() {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById("project-input")
+    );
+
+    this.hostElement = <HTMLDivElement>document.getElementById("app");
+
+    const htmlContent = document.importNode(this.templateElement.content, true);
+
+    this.element = <HTMLFormElement>htmlContent.firstElementChild;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projectInputInstance = new ProjectInput();
