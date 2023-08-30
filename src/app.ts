0de790a29c370a28ce03171e13bc241dbc0fbfd9
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-08-29 21:37:48 -0300

    feat: add form input elements and configure & submitHandler methods

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 class ProjectInput {
+  // Main Elements
   element: HTMLFormElement;
   hostElement: HTMLDivElement;
   templateElement: HTMLTemplateElement;
+
+  // Inputs
+  titleInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLTextAreaElement;
 
   constructor() {
     this.templateElement = <HTMLTemplateElement>(
@@ -13,7 +19,39 @@ class ProjectInput {
     const htmlContent = document.importNode(this.templateElement.content, true);
 
     this.element = <HTMLFormElement>htmlContent.firstElementChild;
+    this.element.setAttribute("id", "user-input");
+
+    this.titleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#title")
+    );
+
+    this.peopleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#people")
+    );
+
+    this.descriptionInputElement = <HTMLTextAreaElement>(
+      this.element.querySelector("#description")
+    );
+
+    // Call methods
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: SubmitEvent) {
+    event.preventDefault();
+
+    console.table({
+      title: this.titleInputElement.value,
+      description: this.descriptionInputElement.value,
+      people: this.peopleInputElement.value,
+    });
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", (event) =>
+      this.submitHandler(event)
+    );
   }
 
   private attach() {
