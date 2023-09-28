663a29900d53184929bb9355afe65550eee18bc8
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-09-28 20:38:20 -0300

    feat: separate project-state

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 /// <reference path="drag-drop-interfaces.ts" />
 /// <reference path="project-model.ts" />
+/// <reference path="project-state.ts" />
 
 namespace App {
   type Validatable = {
@@ -12,8 +13,6 @@ namespace App {
   };
 
   type ElementId = "active" | "finished";
-  type Projects = Project[];
-  type Listener<T> = (projects: T[]) => void;
 
   // type Listeners = Listener[];
 
@@ -27,11 +26,6 @@ namespace App {
   type ProjectItemDTO = {
     project: Project;
     hostElementId: string;
-  };
-
-  type MoveProjectDTO = {
-    projectId: string;
-    status: ProjectStatus;
   };
 
   // Decorators
@@ -143,76 +137,6 @@ namespace App {
     abstract configure(): void;
     abstract renderContent(): void;
   }
-
-  // State Base Class
-  abstract class State<T> {
-    protected listeners: Listener<T>[] = [];
-
-    constructor() {}
-
-    public addListener(listenerFunction: Listener<T>) {
-      this.listeners.push(listenerFunction);
-    }
-  }
-
-  // Project State Management Class
-  class ProjectState extends State<Project> {
-    //private listeners: Listeners = []; // listeners list
-    private projects: Projects = [];
-    private static instance: ProjectState;
-
-    private constructor() {
-      super();
-    }
-
-    static getSingleInstance() {
-      if (!this.instance) {
-        this.instance = new ProjectState();
-        return this.instance;
-      }
-
-      return this.instance;
-    }
-
-    public addProject(project: Omit<Project, "id">): void {
-      const id = Math.random().toString();
-      const { title, people, description, status } = project;
-
-      const newProject: Project = {
-        id,
-        title,
-        description,
-        people,
-        status,
-      };
-
-      this.projects.push(newProject);
-      this.updateListeners();
-    }
-
-    public moveProject(props: MoveProjectDTO): void {
-      const project =
-        this.projects.find((project) => project.id === props.projectId) ?? null;
-
-      if (!project) return;
-      if (project.status === props.status) return;
-
-      project.status = props.status;
-      this.updateListeners();
-    }
-
-    private updateListeners(): void {
-      for (const listener of this.listeners) {
-        // slice return a copy of array
-        // this will be avoid bugs with the projects state
-        // because will be a unique state for each
-        listener(this.projects.slice());
-      }
-    }
-  }
-
-  // global constant instantiating ProjectState class
-  const projectState = ProjectState.getSingleInstance();
 
   // Project Input Class
   class ProjectInput extends Component<HTMLFormElement, HTMLDivElement> {
